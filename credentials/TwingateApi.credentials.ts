91cefe2714506639a5ccfe88fee67aaaf50cc90b
SergeyGers <gers7777@gmail.com>
2025-10-31 22:21:26 +0200

    Update TwingateApi.credentials.ts

--- credentials/TwingateApi.credentials.ts
+++ credentials/TwingateApi.credentials.ts
@@ -5,7 +5,7 @@ export class TwingateApi implements ICredentialType {
 
 	displayName = 'Twingate API';
 
-	icon = 'file:twingate.svg';
+	icon = 'file:twingate.svg' as const;
 
 	documentationUrl = 'https://www.twingate.com/docs/api';
 
